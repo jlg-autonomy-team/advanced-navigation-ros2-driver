125d823d4362c855cee0ca0df21f2aa54056f64c
samfoster <sam.foster@advancednavigation.com>
2026-06-05 13:32:40 +0800

    Fix up launch py files and yaml files

--- adnav_launch/launch/adnav_udp_client.launch.py
+++ adnav_launch/launch/adnav_udp_client.launch.py
@@ -1,28 +1,26 @@
+"""Launch the adnav_driver as a UDP client."""
 import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    ld = LaunchDescription()
 
+def generate_launch_description():
     config = os.path.join(
         get_package_share_directory('adnav_launch'),
         'config',
-        'adnav_udp_client.yaml'
-        )
-        
-    node=Node(
-        name = 'adnav_node',
-        package = 'adnav_driver',
-        executable = 'adnav_driver',
-        emulate_tty = True,
-        output = 'screen',
-        # arguments=['--ros-args', '--log-level', 'debug'],
-        parameters = [config]
+        'adnav_udp_client.yaml',
     )
 
-    ld.add_action
+    node = Node(
+        name='adnav_node',
+        package='adnav_driver',
+        executable='adnav_driver',
+        emulate_tty=True,
+        output='screen',
+        # arguments=['--ros-args', '--log-level', 'debug'],
+        parameters=[config],
+    )
 
-    ld.add_action(node)
-    return ld
+    return LaunchDescription([node])
